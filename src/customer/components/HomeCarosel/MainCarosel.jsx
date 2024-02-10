188da3e7a97156be84c2fb17bbf868cb4f6470c2
saurabhh-svg <singhsaurabh2901@gmail.com>
2024-02-10 23:25:11 +0530

    Carosel implemented

---
+++ src/customer/components/HomeCarosel/MainCarosel.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { mainCarouselData } from "./MainCaroselData";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const MainCarosel = () => {
+  const items = mainCarouselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt=""
+    />
+  ));
+
+  return (
+    <div>
+      <AliceCarousel
+        items={items}
+        disableButtonsControls
+        autoPlay
+        autoPlayInterval={1000}
+        infinite
+      />
+    </div>
+  );
+};
+
+export default MainCarosel;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainCarosel from "../../components/HomeCarosel/MainCarosel";
+
+function HomePage() {
+  return (
+    <div>
+      <MainCarosel />
+    </div>
+  );
+}
+
+export default HomePage;
